4f39e032d4a6327d1e0125b9c1d2a3c3e91abf82
Renê Soares <31143383+Reds74@users.noreply.github.com>
2022-09-15 13:43:06 -0300

    Add files via upload

---
+++ modelos.js
@@ -0,0 +1,10 @@
+const usuarios = [
+    {id: 1, nome: 'Jose', bonus: 500},
+    {id: 2, nome: 'João', bonus: 100},
+    {id: 3, nome: 'Lucas', bonus: 690},
+    {id: 4, nome: 'Maria', bonus: 1000},
+]
+const result = usuarios.filter(usuario => {
+    return usuario.bonus >= 690;
+})
+console.log(result);
